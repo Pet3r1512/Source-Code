86b2c1348cf414f64d4df98ffe8d1cee8dde4c00
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2021-11-02 10:29:36 +0700

    Update IntLinkedList.java

--- Midterm/Cau1.java/IntLinkedList.java
+++ Midterm/Cau1.java/IntLinkedList.java
@@ -36,20 +36,6 @@ public class IntLinkedList implements ListInterface{
         System.out.println("");
     }
 
-    // @Override
-    // public boolean addAfterFirstKey(int data, int key){
-    //     Node tmp  = new Node(data, head), curr = head;
-    //     while(curr.getNext() != null){
-    //         if(curr.getData() == key){
-    //             tmp.setNext(curr.getNext());
-    //             curr.setNext(tmp);
-    //             return true;
-    //         }
-    //         curr =  curr.getNext();
-    //     }
-    //     return false;
-    // }
-
     @Override
     public boolean addAfterFirstKey(int data, int key){//2 3 4 9 5        (9,4) key = 4, data = 9
         Node tmp = new Node(data, head);
@@ -84,22 +70,21 @@ public class IntLinkedList implements ListInterface{
 
     @Override
     public void sort(){
-        Node curr = head, index = null;
-        int tmp;
         if(head == null){
-            throw new NoSuchElementException("Can't sort an empty list");
+            throw new NoSuchElementException("The list is empty");
         }
-        while(curr != null){
-            index = curr.getNext();// index = gia tri next cua curr
-            while(index != null){
-                if(compare(curr.getData(), index.getData()) == -1){
-                    tmp = curr.getData();//tmp = curr
-                    curr.setData(index.getData());//curr = index
-                    index.setData(tmp);//index = tmp
+        Node curr = head, index = null;
+        while(curr.getNext() != null){
+            index = curr.getNext();
+            while(index.getNext() != null){
+                if(compare(curr.getData(), index.getData()) > 0){
+                    int tmp = curr.getData();
+                    curr.setData(index.getData());
+                    index.setData(tmp);
                 }
-                index = index.getNext();//index++
+                index = index.getNext();
             }
-            curr = curr.getNext();//curr++
+            curr = curr.getNext();
         }
     }
 }
