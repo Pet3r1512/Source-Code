025fb449f4bee7b47f385976189dd4a7e5a375df
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2021-11-03 09:01:38 +0700

    Update ListInterface.java

--- MyMidtermTest/ListInterface.java
+++ MyMidtermTest/ListInterface.java
@@ -2,8 +2,8 @@ public interface ListInterface {
     public Node getHead();
     public void addFirst(int data);
     public void print();
-    public boolean addAfterFirstKey(int data, int key);
-    public int compare(int item1, int item2);
-    public void sort();
+//     public boolean addAfterFirstKey(int data, int key);
+//     public int compare(int item1, int item2);
+//     public void sort();
     public int search(int key);
 }
