8d33dad175fb2dfeca3f25bfe30dc0db1d1f7231
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2021-11-04 10:01:44 +0700

    Update ListInterface.java

--- Midterm/Cau1.java/ListInterface.java
+++ Midterm/Cau1.java/ListInterface.java
@@ -5,4 +5,7 @@ interface  ListInterface {
     public boolean addAfterFirstKey(int data, int key);
     public int compare(int item1, int item2);
     public void sort();
+    public void removeAfter(int curr);
+    public void removeFirst();
+    public void removeLast();
 }
