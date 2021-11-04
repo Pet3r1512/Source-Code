9f38f8d91f485bbf3ce8977a10c1dd523e73cdfd
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2021-11-04 14:20:10 +0700

    Update IntLinkedList.java

--- MyMidtermTest/IntLinkedList.java
+++ MyMidtermTest/IntLinkedList.java
@@ -30,53 +30,6 @@ public class IntLinkedList implements ListInterface{
         System.out.println("");
     }
 
-//     public boolean addAfterFirstKey(int data, int key){
-//         if(head == null){
-//             throw new NoSuchElementException("The list is empty");
-//         }
-//         Node tmp = new Node(data, head);
-//         for(Node curr = head; curr.getNext() != null; curr = curr.getNext()){
-//             if(curr.getData() == key){
-//                 tmp.setNext(curr.getNext());
-//                 curr.setNext(tmp);
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-
-//     public int compare(int item1, int item2){
-//         int tmp = item1 - item2;
-//         if(tmp > 0){
-//             return 1;
-//         }
-//         else if( tmp < 0){
-//             return -1;
-//         }
-//         else{
-//             return 0;
-//         }
-//     }
-
-//     public void sort(){
-//         if(head == null){
-//             throw new NoSuchElementException("The list is empty");
-//         }
-//         Node curr = head, index = null;
-//         while(curr.getNext() != null){
-//             index = curr.getNext();
-//             while(index.getNext() != null){
-//                 if(compare(curr.getData(), index.getData()) > 0){
-//                     int tmp = curr.getData();
-//                     curr.setData(index.getData());
-//                     index.setData(tmp);
-//                 }
-//                 index = index.getNext();
-//             }
-//             curr = curr.getNext();
-//         }
-//     }
-
     public int search(int key){
         if(head == null){
             throw new NoSuchElementException("Can't search in an empty list");
